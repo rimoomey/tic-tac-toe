ca289063d2784f7025c842c194e8fd04d8c8bb33
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-12 08:24:29 -0500

    Refactor Board using binary board state.

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,28 +1,27 @@
 # Board for tic tac toe game
 class Board
-  attr_reader :els
+  attr_accessor :player1_state, :player2_state
 
-  def initialize
-    @els = {}
+  WIN_STATES = [
+    0b111000000,
+    0b000111000,
+    0b000000111,
+    0b100010001,
+    0b001010100,
+    0b100100100,
+    0b001001001
+  ]
+
+  def initialize(player1_state = 0, player2_state = 0)
+    @player1_state = player1_state
+    @player2_state = player2_state
+  end
+
+  def check_win(player1_state, player2_state)
+    WIN_STATES.each do |state|
+      return 1 if player1_state & state == state
+      return -1 if player2_state & state == state
+    end
+    0
   end
 end
-
-# Square meant to hold a place in tic-tac-toe board
-class Square
-  attr_accessor :state
-
-  def initialize(state = '')
-    @state = state
-  end
-end
-
-game_board = Board.new
-
-(1..3).each do |left_index|
-  (1..3).each do |right_index|
-    box = Square.new
-    game_board.els["#{left_index}, #{right_index}"] = box
-  end
-end
-
-game_board.els.each { |hash| p hash }
